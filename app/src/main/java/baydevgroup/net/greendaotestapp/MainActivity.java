a96d454561636b519ec005d70cff6bb9686f29f6
Chingiz-888 <chingiz888@mail.ru>
2017-06-04 00:04:13 +0300

    включение GreenDao

--- app/src/main/java/baydevgroup/net/greendaotestapp/MainActivity.java
+++ app/src/main/java/baydevgroup/net/greendaotestapp/MainActivity.java
@@ -3,11 +3,26 @@ package baydevgroup.net.greendaotestapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.facebook.stetho.Stetho;
+
+import org.greenrobot.greendao.database.Database;
+
+import model.DaoMaster;
+import model.DaoSession;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //TODO: надо это переместить в Application класс
+        DaoMaster.DevOpenHelper helper = new DaoMaster.DevOpenHelper(this, "test-db");
+        Database db = helper.getReadableDb();
+        DaoSession sDaoSession = new DaoMaster(db).newSession();
+
+        // Для отладки через Chrome DevTools
+        Stetho.initializeWithDefaults(this);
     }
 }
---
+++ app/src/main/java/model/User.java
@@ -0,0 +1,96 @@
+package model;
+
+import org.greenrobot.greendao.annotation.Entity;
+import org.greenrobot.greendao.annotation.Id;
+import org.greenrobot.greendao.annotation.Generated;
+import org.greenrobot.greendao.DaoException;
+
+/**
+ * Created by cingiz-mac on 03.06.17.
+ */
+
+@Entity(active = true, nameInDb = "USERS")
+public class User {
+
+    @Id
+    private Long id;
+
+    private String name;
+
+    /** Used to resolve relations */
+    @Generated(hash = 2040040024)
+    private transient DaoSession daoSession;
+
+    /** Used for active entity operations. */
+    @Generated(hash = 1507654846)
+    private transient UserDao myDao;
+
+    @Generated(hash = 873297011)
+    public User(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    @Generated(hash = 586692638)
+    public User() {
+    }
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * Convenient call for {@link org.greenrobot.greendao.AbstractDao#delete(Object)}.
+     * Entity must attached to an entity context.
+     */
+    @Generated(hash = 128553479)
+    public void delete() {
+        if (myDao == null) {
+            throw new DaoException("Entity is detached from DAO context");
+        }
+        myDao.delete(this);
+    }
+
+    /**
+     * Convenient call for {@link org.greenrobot.greendao.AbstractDao#refresh(Object)}.
+     * Entity must attached to an entity context.
+     */
+    @Generated(hash = 1942392019)
+    public void refresh() {
+        if (myDao == null) {
+            throw new DaoException("Entity is detached from DAO context");
+        }
+        myDao.refresh(this);
+    }
+
+    /**
+     * Convenient call for {@link org.greenrobot.greendao.AbstractDao#update(Object)}.
+     * Entity must attached to an entity context.
+     */
+    @Generated(hash = 713229351)
+    public void update() {
+        if (myDao == null) {
+            throw new DaoException("Entity is detached from DAO context");
+        }
+        myDao.update(this);
+    }
+
+    /** called by internal mechanisms, do not call yourself. */
+    @Generated(hash = 2059241980)
+    public void __setDaoSession(DaoSession daoSession) {
+        this.daoSession = daoSession;
+        myDao = daoSession != null ? daoSession.getUserDao() : null;
+    }
+}
